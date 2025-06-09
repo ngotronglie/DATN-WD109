d791af91e3c8fa7b9c00c9da1ca6dd2198ea0182
quanghuy14092003 <quanghuy14923@gmail.com>
2025-06-09 17:36:43 +0700

    CRUD roles

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\RoleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,4 +30,13 @@ Route::prefix('admin/categories')->name('admin.categories.')->group(function () 
     Route::view('/update', 'layouts.admin.category.update')->name('update');
 });
 
+// Nhóm routes cho quản lý roles
+Route::prefix('admin/roles')->name('admin.roles.')->group(function () {
+    Route::get('/', [RoleController::class, 'index'])->name('index');          // danh sách role
+    Route::get('/create', [RoleController::class, 'create'])->name('create');  // form tạo mới
+    Route::post('/', [RoleController::class, 'store'])->name('store');         // lưu role mới
+    Route::get('/{id}/edit', [RoleController::class, 'edit'])->name('edit');   // form chỉnh sửa role
+    Route::put('/{id}', [RoleController::class, 'update'])->name('update');    // cập nhật role
+    Route::delete('/{id}', [RoleController::class, 'destroy'])->name('destroy');// xóa role
+});
 // end route admin
