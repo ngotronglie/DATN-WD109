be63bf48acc0ee7dc127f10e97ad1d677efc66a5
Liemnt <ngotrongliem2004@gmail.com>
2025-07-01 21:43:31 +0700

    sửa lại script admin

--- resources/views/components/admin/script.blade.php
+++ resources/views/components/admin/script.blade.php
@@ -12,17 +12,48 @@
 <!-- Dashboard Js -->
 <!-- <script src="{{ asset('dashboard/assets/js/pages/dashboard.js') }}"></script> -->
 
-@yield('script')
+@yield('scripts')
 
 <script>
-
-                    
-          // Snow theme
-          var quill = new Quill('#snow-editor', {
-               theme: 'snow',
-               modules: {
-                    'toolbar': [[{ 'font': [] }, { 'size': [] }], ['bold', 'italic', 'underline', 'strike'], [{ 'color': [] }, { 'background': [] }], [{ 'script': 'super' }, { 'script': 'sub' }], [{ 'header': [false, 1, 2, 3, 4, 5, 6] }, 'blockquote', 'code-block'], [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }], ['direction', { 'align': [] }], ['link', 'image', 'video'], ['clean']]
-               },
-          });
-
+    // Snow theme
+    var quill = new Quill('#snow-editor', {
+        theme: 'snow',
+        modules: {
+            'toolbar': [
+                [{
+                    'font': []
+                }, {
+                    'size': []
+                }],
+                ['bold', 'italic', 'underline', 'strike'],
+                [{
+                    'color': []
+                }, {
+                    'background': []
+                }],
+                [{
+                    'script': 'super'
+                }, {
+                    'script': 'sub'
+                }],
+                [{
+                    'header': [false, 1, 2, 3, 4, 5, 6]
+                }, 'blockquote', 'code-block'],
+                [{
+                    'list': 'ordered'
+                }, {
+                    'list': 'bullet'
+                }, {
+                    'indent': '-1'
+                }, {
+                    'indent': '+1'
+                }],
+                ['direction', {
+                    'align': []
+                }],
+                ['link', 'image', 'video'],
+                ['clean']
+            ]
+        },
+    });
 </script>
