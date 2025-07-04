b5853b0656372d4d8657d91a433701cb5da54e2e
sang2505 <nguyensang.25052004@gmail.com>
2025-07-04 23:21:30 +0700

    hien thi anh ,tieu de,laays tu db

--- resources/views/components/client/banner.blade.php
+++ resources/views/components/client/banner.blade.php
@@ -20,11 +20,11 @@
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span class="visually-hidden">Trước</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span class="visually-hidden">Tiếp Theo</span>
     </button>
 </div>
 
