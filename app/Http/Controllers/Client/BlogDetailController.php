d98dd017b0100e74126388a652dbf154a474e0ce
sang2505 <nguyensang.25052004@gmail.com>
2025-07-05 12:00:45 +0700

    them chuc nang hien thi chi tiet blog

--- app/Http/Controllers/Client/BlogDetailController.php
+++ app/Http/Controllers/Client/BlogDetailController.php
@@ -29,5 +29,42 @@ class BlogDetailController extends Controller
         
         return view('layouts.user.blog', compact('blogs', 'recentBlogs', 'tags'));
     }
-    
+      /**
+     * Hiển thị chi tiết một blog
+     */
+    public function show($slug)
+    {
+        $blog = Blog::with(['user', 'tags'])
+            ->where('slug', $slug)
+            ->where('is_active', true)
+            ->firstOrFail();
+            
+        // Tăng lượt xem
+        $blog->increment('view');
+        
+        // Lấy blog liên quan
+        $relatedBlogs = Blog::with('user')
+            ->where('is_active', true)
+            ->where('id', '!=', $blog->id)
+            ->whereHas('tags', function($query) use ($blog) {
+                $query->whereIn('tag_blog.id', $blog->tags->pluck('id'));
+            })
+            ->orWhere('user_id', $blog->user_id)
+            ->limit(3)
+            ->get();
+            
+        // Lấy blog gần đây
+        $recentBlogs = Blog::with('user')
+            ->where('is_active', true)
+            ->where('id', '!=', $blog->id)
+            ->orderBy('created_at', 'desc')
+            ->limit(5)
+            ->get();
+            
+        // Lấy tất cả tags
+        $tags = TagBlog::withCount('blogs')->get();
+        
+        return view('layouts.user.blogDetail', compact('blog', 'relatedBlogs', 'recentBlogs', 'tags'));
+    }
+
 }
