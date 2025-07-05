cdff9efc627b260638ec1821b892830e7f2f7399
sang2505 <nguyensang.25052004@gmail.com>
2025-07-05 13:11:17 +0700

    view blog hien thi tu db

--- resources/views/layouts/user/blogDetail.blade.php
+++ resources/views/layouts/user/blogDetail.blade.php
@@ -1,5 +1,9 @@
 @extends('index.clientdashboard')
 
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/blog-custom.css') }}">
+@endsection
+
 @section('content')
 
         <!-- BREADCRUMBS SETCTION START -->
@@ -33,38 +37,60 @@
                             <div class="blog-details-area">
                                 <!-- blog-details-photo -->
                                 <div class="blog-details-photo bg-img-1 p-20 mb-30">
-                                    <img src="{{asset('frontend/img/blog/10.jpg')}}" alt="">
+                                    <img src="{{ $blog->image ? asset($blog->image) : asset('frontend/img/blog/10.jpg') }}" alt="{{ $blog->slug }}">
                                     <div class="today-date bg-img-1">
-                                        <span class="meta-date">30</span>
-                                        <span class="meta-month">June</span>
+                                        <span class="meta-date">{{ $blog->created_at->format('d') }}</span>
+                                        <span class="meta-month">{{ $blog->created_at->format('M') }}</span>
                                     </div>
                                 </div>
                                 <!-- blog-like-share -->
                                 <ul class="blog-like-share mb-20">
                                     <li>
-                                        <a href="#"><i class="zmdi zmdi-favorite"></i>89 Like</a>
+                                        <a href="#"><i class="zmdi zmdi-eye"></i>{{ $blog->view }} Views</a>
                                     </li>
                                     <li>
-                                        <a href="#"><i class="zmdi zmdi-comments"></i>59 Comments</a>
+                                        <a href="#"><i class="zmdi zmdi-account"></i>{{ $blog->user->name ?? 'Admin' }}</a>
                                     </li>
                                     <li>
-                                        <a href="#"><i class="zmdi zmdi-share"></i>29 Share</a>
+                                        <a href="#"><i class="zmdi zmdi-calendar"></i>{{ $blog->created_at->format('d/m/Y') }}</a>
                                     </li>
                                 </ul>
                                 <!-- blog-details-title -->
-                                <h3 class="blog-details-title mb-30">Dumm`y single blog name</h3>
+                                <h3 class="blog-details-title mb-30">{{ $blog->slug }}</h3>
+                                
+                                @auth
+                                    @if(Auth::user()->is_admin)
+                                        <div class="admin-actions mb-30">
+                                            <a href="{{ route('blog.detail.edit', $blog->slug) }}" class="btn btn-primary btn-sm">
+                                                <i class="zmdi zmdi-edit"></i> Chỉnh sửa
+                                            </a>
+                                            <form action="{{ route('blog.detail.destroy', $blog->slug) }}" method="POST" class="d-inline" 
+                                                  onsubmit="return confirm('Bạn có chắc chắn muốn xóa bài viết này?')">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm">
+                                                    <i class="zmdi zmdi-delete"></i> Xóa
+                                                </button>
+                                            </form>
+                                        </div>
+                                    @endif
+                                @endauth
                                 <!-- blog-description -->
                                 <div class="blog-description mb-60">
-                                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of plea sure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belon gs to those who fail in their duty through weakness of will, which is the same as saying through shrink ing from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour.</p>
-
-                                    <div class="quote-author pl-30">
-                                        <p class="quote-border pl-30">On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charm of pleas ure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will</p>
-                                    </div>
-
-                                    <p>Which is the same as saying through shrink ing from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour. One the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of plea sure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belong to those who fail in their duty through weakness of will, which is the same as saying through shrink ing from toil and pain. These cases are perfectly simple and easy to distinguish.</p>
-
-                                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of plea sure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belon gs to those who fail in their duty through weakness of will, which is the same as saying through shrink ing from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour.</p>
+                                    {!! $blog->content !!}
                                 </div>
+                                
+                                <!-- blog tags -->
+                                @if($blog->tags->count() > 0)
+                                <div class="blog-tags mb-30">
+                                    <h4>Tags:</h4>
+                                    <div class="tags-list">
+                                        @foreach($blog->tags as $tag)
+                                            <a href="{{ route('blog.detail.tag', $tag->id) }}" class="tag-item">{{ $tag->name_tag }}</a>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                @endif
                                 <!-- blog-share-tags -->
                                 <div class="blog-share-tags box-shadow clearfix mb-60">
                                     <div class="blog-share f-left">
@@ -87,10 +113,11 @@
                                     <div class="blog-tags f-right">
                                         <p class="share-tags-title f-left">Tags</p>
                                         <ul class="blog-tags-list f-left">
-                                            <li><a href="#">Mobile</a></li>
-                                            <li><a href="#">IOS</a></li>
-                                            <li><a href="#">Windows</a></li>
-                                            <li><a href="#">Tab</a></li>
+                                            @forelse($blog->tags as $tag)
+                                                <li><a href="{{ route('blog.detail.tag', $tag->id) }}">{{ $tag->name_tag }}</a></li>
+                                            @empty
+                                                <li><span>Không có tags</span></li>
+                                            @endforelse
                                         </ul>
                                     </div>
                                 </div>
@@ -98,14 +125,14 @@
                                 <div class="media author-post box-shadow mb-60">
                                     <div class="media-left pr-20">
                                         <a href="#">
-                                            <img class="media-object" src="{{asset('frontend/img/author/1.jpg')}}" alt="#">
+                                            <img class="media-object" src="{{asset('frontend/img/author/1.jpg')}}" alt="{{ $blog->user->name ?? 'Admin' }}">
                                         </a>
                                     </div>
                                     <div class="media-body">
                                         <h6 class="media-heading">
-                                            <a href="#">Subas Chandra Das</a>
+                                            <a href="#">{{ $blog->user->name ?? 'Admin' }}</a>
                                         </h6>
-                                        <p class="mb-0">No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursu pleasure rationally encounter conseques ences that are extremely painful.</p>
+                                        <p class="mb-0">Tác giả của bài viết này.</p>
                                     </div>
                                 </div>
                                 <!-- comments on t this post -->
@@ -177,75 +204,43 @@
                         <div class="col-lg-3">
                             <!-- widget-search -->
                             <aside class="widget-search mb-30">
-                                <form action="#">
-                                    <input type="text" placeholder="Search here...">
+                                <form action="{{ route('blog.detail.search') }}" method="GET">
+                                    <input type="text" name="keyword" placeholder="Tìm kiếm bài viết..." 
+                                           value="{{ $keyword ?? '' }}">
                                     <button type="submit"><i class="zmdi zmdi-search"></i></button>
                                 </form>
                             </aside>
                             <!-- widget-categories -->
                             <aside class="widget widget-categories box-shadow mb-30">
-                                <h6 class="widget-title border-left mb-20">Categories</h6>
+                                <h6 class="widget-title border-left mb-20">Tags</h6>
                                 <div id="cat-treeview" class="product-cat">
                                     <ul>
-                                        <li class="closed"><a href="#">Brand One</a>
-                                            <ul>
-                                                <li><a href="#">Mobile</a></li>
-                                                <li><a href="#">Tab</a></li>
-                                                <li><a href="#">Watch</a></li>
-                                                <li><a href="#">Head Phone</a></li>
-                                                <li><a href="#">Memory</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="open"><a href="#">Brand Two</a>
-                                            <ul>
-                                                <li><a href="#">Mobile</a></li>
-                                                <li><a href="#">Tab</a></li>
-                                                <li><a href="#">Watch</a></li>
-                                                <li><a href="#">Head Phone</a></li>
-                                                <li><a href="#">Memory</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="closed"><a href="#">Accessories</a>
-                                            <ul>
-                                                <li><a href="#">Footwear</a></li>
-                                                <li><a href="#">Sunglasses</a></li>
-                                                <li><a href="#">Watches</a></li>
-                                                <li><a href="#">Utilities</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="closed"><a href="#">Top Brands</a>
-                                            <ul>
-                                                <li><a href="#">Mobile</a></li>
-                                                <li><a href="#">Tab</a></li>
-                                                <li><a href="#">Watch</a></li>
-                                                <li><a href="#">Head Phone</a></li>
-                                                <li><a href="#">Memory</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="closed"><a href="#">Jewelry</a>
-                                            <ul>
-                                                <li><a href="#">Footwear</a></li>
-                                                <li><a href="#">Sunglasses</a></li>
-                                                <li><a href="#">Watches</a></li>
-                                                <li><a href="#">Utilities</a></li>
-                                            </ul>
-                                        </li>
+                                        @forelse($tags as $tag)
+                                            <li>
+                                                <a href="{{ route('blog.detail.tag', $tag->id) }}">
+                                                    {{ $tag->name_tag }} ({{ $tag->blogs_count }})
+                                                </a>
+                                            </li>
+                                        @empty
+                                            <li><span>Không có tags</span></li>
+                                        @endforelse
                                     </ul>
                                 </div>
                             </aside>
                             <!-- widget-product -->
                             <aside class="widget widget-product box-shadow mb-30">
-                                <h6 class="widget-title border-left mb-20">recent products</h6>
+                                <h6 class="widget-title border-left mb-20">recent blogs</h6>
+                                @forelse($recentBlogs as $recentBlog)
                                 <!-- product-item start -->
                                 <div class="product-item">
                                     <div class="product-img">
-                                        <a href="single-product.html">
-                                            <img src="{{asset('frontend/img/product/4.jpg')}}" alt=""/>
+                                        <a href="{{ route('blog.detail.show', $recentBlog->slug) }}">
+                                            <img src="{{ $recentBlog->image ? asset($recentBlog->image) : asset('frontend/img/blog/1.jpg') }}" alt="{{ $recentBlog->slug }}"/>
                                         </a>
                                     </div>
                                     <div class="product-info">
                                         <h6 class="product-title">
-                                            <a href="single-product.html">Product Name</a>
+                                            <a href="{{ route('blog.detail.show', $recentBlog->slug) }}">{{ $recentBlog->slug }}</a>
                                         </h6>
                                         <h3 class="pro-price">$ 869.00</h3>
                                     </div>
