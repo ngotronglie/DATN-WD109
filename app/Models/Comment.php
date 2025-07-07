26ce914f26ad6fcb7e63f14da573d758177934a4
sang2505 <nguyensang.25052004@gmail.com>
2025-07-07 17:11:23 +0700

    tao model cho comment blog

---
+++ app/Models/Comment.php
@@ -0,0 +1,28 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['blog_id', 'user_id', 'content', 'parent_id'];
+
+    public function blog()
+    {
+        return $this->belongsTo(Blog::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
