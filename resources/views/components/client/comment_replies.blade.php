6630346f4279dce7d9346fdcb139c84601443218
sang2505 <nguyensang.25052004@gmail.com>
2025-07-19 16:03:24 +0700

    xu li tra loi binh luan trong controller

--- resources/views/components/client/comment_replies.blade.php
+++ resources/views/components/client/comment_replies.blade.php
@@ -3,7 +3,6 @@
         <strong>{{ $comment->user->name }}</strong>
         <p>{{ $comment->content }}</p>
         <a href="#" class="reply-btn" data-id="{{ $comment->id }}">Trả lời</a>
-        {{-- Form trả lời (ẩn/hiện bằng JS) --}}
         <div class="reply-form" id="reply-form-{{ $comment->id }}" style="display:none;">
             <form method="POST" action="{{ route('comments.reply', $comment->id) }}">
                 @csrf
@@ -11,7 +10,6 @@
                 <button type="submit">Gửi</button>
             </form>
         </div>
-        {{-- Hiển thị replies --}}
         @if($comment->replies)
             <div class="replies" style="margin-left: 40px;">
                 @include('components.comment_replies', ['comments' => $comment->replies])
