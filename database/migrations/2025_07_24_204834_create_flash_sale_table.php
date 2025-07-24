380e57fc65d928a2da65decdcf9cca476ec784f3
huongntph35323 <huongntph35323@fpt.edu.vn>
2025-07-24 20:52:00 +0700

    tạo bảng fashsale

---
+++ database/migrations/2025_07_24_204834_create_flash_sale_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flash_sale', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->decimal('sale_price', 10, 2);
+            $table->integer('sale_quantity');
+            $table->datetime('start_time');
+            $table->datetime('end_time');
+            $table->enum('trang_thai', ['kích_hoạt', 'tạm_dừng'])->default('kích_hoạt');
+            $table->timestamps(); // Tự động thêm created_at và updated_at
+
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flash_sale');
+    }
+};
